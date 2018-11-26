a4ec1c426088d7066bbe711acb26b2510ffd7db4
Jimmi HC <jimmiholstchristensen@gmail.com>
2018-11-26 18:31:21 +0100

    Simplified using the new matching api

--- src/main.zig
+++ src/main.zig
@@ -117,8 +117,119 @@ pub fn main2(allocator: *mem.Allocator, args: Clap, stdin: var, stdout: var, std
     var arena = heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
 
-    const pokemons = try Parser.parse(&arena.allocator, stdin, stdout);
+    const pokemons = try readPokemons(&arena.allocator, stdin, stdout);
+    randomize(pokemons, seed, same_total_stats);
 
+    var iter = pokemons.iterator();
+    while (iter.next()) |kv| {
+        inline for (stats) |stat| {
+            if (@field(kv.value, stat)) |s|
+                try stdout.print("pokemons[{}].stats.{}={}\n", kv.key, stat, s);
+        }
+    }
+}
+
+fn readPokemons(allocator: *mem.Allocator, in_stream: var, out_stream: var) !PokemonMap {
+    @setEvalBranchQuota(100000);
+
+    const m = format.Matcher([][]const u8{
+        "pokemons[*].stats.hp",
+        "pokemons[*].stats.attack",
+        "pokemons[*].stats.defense",
+        "pokemons[*].stats.speed",
+        "pokemons[*].stats.sp_attack",
+        "pokemons[*].stats.sp_defense",
+        "pokemons[*].evos[*].target",
+    });
+
+    var res = PokemonMap.init(allocator);
+    var line_buf = try std.Buffer.initSize(allocator, 0);
+    defer line_buf.deinit();
+
+    var line: usize = 1;
+    while (readLine(in_stream, &line_buf)) |str| : (line += 1) {
+        if (m.match(str)) |match| switch (match.case) {
+            m.case("pokemons[*].stats.hp"),
+            m.case("pokemons[*].stats.attack"),
+            m.case("pokemons[*].stats.defense"),
+            m.case("pokemons[*].stats.speed"),
+            m.case("pokemons[*].stats.sp_attack"),
+            m.case("pokemons[*].stats.sp_defense"),
+            => success: {
+                err: {
+                    const value = fmt.parseUnsigned(u8, mem.trim(u8, match.value.str, "\t "), 10) catch break :err;
+                    const index = fmt.parseUnsigned(usize, match.anys[0].str, 10) catch break :err;
+
+                    const entry = try res.getOrPut(index);
+                    const pokemon = &entry.kv.value;
+                    if (!entry.found_existing) {
+                        pokemon.* = Pokemon{
+                            .hp = null,
+                            .attack = null,
+                            .defense = null,
+                            .speed = null,
+                            .sp_attack = null,
+                            .sp_defense = null,
+                            .evos = EvoMap.init(allocator),
+                        };
+                    }
+
+                    switch (match.case) {
+                        m.case("pokemons[*].stats.hp") => pokemon.hp = value,
+                        m.case("pokemons[*].stats.attack") => pokemon.attack = value,
+                        m.case("pokemons[*].stats.defense") => pokemon.defense = value,
+                        m.case("pokemons[*].stats.speed") => pokemon.speed = value,
+                        m.case("pokemons[*].stats.sp_attack") => pokemon.sp_attack = value,
+                        m.case("pokemons[*].stats.sp_defense") => pokemon.sp_defense = value,
+                        else => unreachable,
+                    }
+
+                    break :success;
+                }
+
+                try out_stream.print("{}\n", str);
+            },
+            m.case("pokemons[*].evos[*].target") => {
+                err: {
+                    const value = fmt.parseUnsigned(u8, mem.trim(u8, match.value.str, "\t "), 10) catch break :err;
+                    const poke_index = fmt.parseUnsigned(usize, match.anys[0].str, 10) catch break :err;
+                    const evo_index = fmt.parseUnsigned(usize, match.anys[1].str, 10) catch break :err;
+
+                    const entry = try res.getOrPut(poke_index);
+                    const pokemon = &entry.kv.value;
+                    if (!entry.found_existing) {
+                        pokemon.* = Pokemon{
+                            .hp = null,
+                            .attack = null,
+                            .defense = null,
+                            .speed = null,
+                            .sp_attack = null,
+                            .sp_defense = null,
+                            .evos = EvoMap.init(allocator),
+                        };
+                    }
+
+                    const value_str = mem.trim(u8, match.value.str, "\t ");
+                    _ = try pokemon.evos.put(evo_index, value);
+                }
+
+                try out_stream.print("{}\n", str);
+            },
+            else => try out_stream.print("{}\n", str),
+        } else |_| {
+            try out_stream.print("{}\n", str);
+        }
+
+        line_buf.shrink(0);
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    }
+
+    return res;
+}
+
+fn randomize(pokemons: PokemonMap, seed: u64, same_total_stats: bool) void {
     var random = rand.DefaultPrng.init(seed);
     var iter = pokemons.iterator();
     while (iter.next()) |kv| {
@@ -156,12 +267,14 @@ pub fn main2(allocator: *mem.Allocator, args: Clap, stdin: var, stdout: var, std
             debug.assert(total == pokemon.totalStats());
         }
     }
+}
 
-    iter = pokemons.iterator();
-    while (iter.next()) |kv| {
-        inline for (stats) |stat| {
-            if (@field(kv.value, stat)) |s|
-                try stdout.print("pokemons[{}].stats.{}={}\n", kv.key, stat, s);
+fn readLine(stream: var, buf: *std.Buffer) ![]u8 {
+    while (true) {
+        const byte = try stream.readByte();
+        switch (byte) {
+            '\n' => return buf.toSlice(),
+            else => try buf.appendByte(byte),
         }
     }
 }
@@ -207,211 +320,3 @@ const Pokemon = struct {
         return p.statCount() * math.maxInt(u8);
     }
 };
-
-const Parser = struct {
-    parser: format.Parser,
-    line: usize,
-
-    fn parse(allocator: *mem.Allocator, in_stream: var, out_stream: var) !PokemonMap {
-        var res = PokemonMap.init(allocator);
-        var line_buf = try std.Buffer.initSize(allocator, 0);
-        defer line_buf.deinit();
-
-        var line: usize = 1;
-        while (readLine(in_stream, &line_buf)) |str| : (line += 1) {
-            var parser = Parser{
-                .parser = format.Parser.init(format.Tokenizer.init(str)),
-                .line = line,
-            };
-
-            switch (parser.parseLine(&res, str) catch PrintLine.True) {
-                PrintLine.True => try out_stream.print("{}\n", str),
-                PrintLine.False => {},
-            }
-
-            line_buf.shrink(0);
-        } else |err| switch (err) {
-            error.EndOfStream => {},
-            else => return err,
-        }
-
-        return res;
-    }
-
-    const PrintLine = enum {
-        True,
-        False,
-    };
-
-    fn parseLine(parser: *Parser, pokemons: *PokemonMap, str: []const u8) !PrintLine {
-        @setEvalBranchQuota(100000);
-
-        const tf = fun.match.StringSwitch([][]const u8{"pokemons"});
-
-        const top_field_node = switch (parser.parser.next() orelse return PrintLine.True) {
-            format.Parser.Result.Ok => |node| node.Field,
-            format.Parser.Result.Error => |err| return parser.reportSyntaxError(err),
-        };
-
-        switch (tf.match(top_field_node.ident.str)) {
-            tf.case("pokemons") => {
-                const pokemon_index = try parser.expectIndex(usize, math.maxInt(usize));
-                const pokemon_field_node = try parser.expect(format.Node.Kind.Field);
-                const pf = fun.match.StringSwitch([][]const u8{
-                    "stats",
-                    "evos",
-                });
-
-                switch (pf.match(pokemon_field_node.ident.str)) {
-                    pf.case("stats") => {
-                        const stats_field_node = try parser.expect(format.Node.Kind.Field);
-                        const sf = fun.match.StringSwitch(stats);
-
-                        const match = sf.match(stats_field_node.ident.str);
-                        const value = switch (match) {
-                            sf.case("hp") => try parser.expectIntValue(u8),
-                            sf.case("attack") => try parser.expectIntValue(u8),
-                            sf.case("defense") => try parser.expectIntValue(u8),
-                            sf.case("speed") => try parser.expectIntValue(u8),
-                            sf.case("sp_attack") => try parser.expectIntValue(u8),
-                            sf.case("sp_defense") => try parser.expectIntValue(u8),
-                            else => return PrintLine.True,
-                        };
-
-                        const entry = try pokemons.getOrPut(pokemon_index);
-                        const pokemon = &entry.kv.value;
-                        if (!entry.found_existing) {
-                            pokemon.* = Pokemon{
-                                .hp = null,
-                                .attack = null,
-                                .defense = null,
-                                .speed = null,
-                                .sp_attack = null,
-                                .sp_defense = null,
-                                .evos = EvoMap.init(pokemons.allocator),
-                            };
-                        }
-
-                        switch (match) {
-                            sf.case("hp") => pokemon.hp = value,
-                            sf.case("attack") => pokemon.attack = value,
-                            sf.case("defense") => pokemon.defense = value,
-                            sf.case("speed") => pokemon.speed = value,
-                            sf.case("sp_attack") => pokemon.sp_attack = value,
-                            sf.case("sp_defense") => pokemon.sp_defense = value,
-                            else => unreachable,
-                        }
-
-                        return PrintLine.False;
-                    },
-                    pf.case("evos") => {
-                        const evo_index = try parser.expectIndex(usize, math.maxInt(usize));
-                        const evo_field_node = try parser.expect(format.Node.Kind.Field);
-                        const ef = fun.match.StringSwitch([][]const u8{
-                            "target",
-                            "HACK TO RESOLVE https://github.com/ziglang/zig/issues/1608",
-                            "1HACK TO RESOLVE https://github.com/ziglang/zig/issues/1608",
-                        });
-
-                        switch (ef.match(evo_field_node.ident.str)) {
-                            ef.case("target") => {
-                                const entry = try pokemons.getOrPut(pokemon_index);
-                                const pokemon = &entry.kv.value;
-                                if (!entry.found_existing) {
-                                    pokemon.* = Pokemon{
-                                        .hp = null,
-                                        .attack = null,
-                                        .defense = null,
-                                        .speed = null,
-                                        .sp_attack = null,
-                                        .sp_defense = null,
-                                        .evos = EvoMap.init(pokemons.allocator),
-                                    };
-                                }
-
-                                const value = try parser.expectIntValue(usize);
-                                _ = try pokemon.evos.put(evo_index, value);
-
-                                return PrintLine.True;
-                            },
-                            else => {},
-                        }
-                    },
-                    else => {},
-                }
-            },
-            else => {},
-        }
-
-        return PrintLine.True;
-    }
-
-    fn expect(parser: *Parser, comptime kind: format.Node.Kind) !@field(format.Node, @tagName(kind)) {
-        switch (parser.parser.next().?) {
-            format.Parser.Result.Ok => |node| switch (node) {
-                kind => |res| return res,
-                else => return error.InvalidNode,
-            },
-            format.Parser.Result.Error => |err| return parser.reportSyntaxError(err),
-        }
-    }
-
-    fn expectIndex(parser: *Parser, comptime Int: type, bound: Int) !Int {
-        const index_node = try parser.expect(format.Node.Kind.Index);
-        return try parser.parseInt(Int, bound, index_node.int);
-    }
-
-    fn expectIntValue(parser: *Parser, comptime Int: type) !Int {
-        const value_node = try parser.expect(format.Node.Kind.Value);
-        return try parser.parseInt(Int, math.maxInt(Int), value_node.value);
-    }
-
-    fn parseInt(parser: *const Parser, comptime Int: type, bound: Int, token: format.Token) !Int {
-        const column = token.index(parser.parser.tok.str) + 1;
-        const str = mem.trim(u8, token.str, "\t ");
-        overflow: {
-            return fmt.parseUnsigned(Int, str, 10) catch |err| {
-                switch (err) {
-                    error.Overflow => break :overflow,
-                    error.InvalidCharacter => {
-                        parser.warning(column, "{} is not an number", str);
-                        return err;
-                    },
-                }
-            };
-        }
-
-        parser.warning(column, "{} is not within the bound {}", str, bound);
-        return error.Overflow;
-    }
-
-    fn reportSyntaxError(parser: *const Parser, err: format.Parser.Error) error{SyntaxError} {
-        parser.warning(err.found.index(parser.parser.tok.str), "expected ");
-        for (err.expected) |id, i| {
-            const rev_i = (err.expected.len - 1) - i;
-            debug.warn("{}", id.str());
-            if (rev_i == 1)
-                debug.warn(" or ");
-            if (rev_i > 1)
-                debug.warn(", ");
-        }
-
-        debug.warn(" found {}", err.found.str);
-        return error.SyntaxError;
-    }
-
-    fn warning(parser: *const Parser, col: usize, comptime f: []const u8, a: ...) void {
-        debug.warn("(stdin):{}:{}: warning: ", parser.line, col);
-        debug.warn(f, a);
-    }
-};
-
-fn readLine(stream: var, buf: *std.Buffer) ![]u8 {
-    while (true) {
-        const byte = try stream.readByte();
-        switch (byte) {
-            '\n' => return buf.toSlice(),
-            else => try buf.appendByte(byte),
-        }
-    }
-}
