75585b179b97862a8927bdba0ed5c4928a6b7177
Jimmi HC <jimmi@ziglang.org>
2019-01-30 20:05:10 +0100

    fixed build on windows

--- build.zig
+++ build.zig
@@ -6,7 +6,6 @@ pub fn build(b: *Builder) void {
     exe.setBuildMode(mode);
     exe.addPackagePath("tm35-nds", "lib/tm35-nds/src/index.zig");
     exe.addPackagePath("zig-clap", "lib/zig-clap/index.zig");
-    exe.linkSystemLibrary("c");
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 }
