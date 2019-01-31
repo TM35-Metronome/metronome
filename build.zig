92a1f4e292a7f87d5a34b588334f1483861e61d8
Jimmi Holst Christensen <jhc@liab.dk>
2019-01-31 10:54:47 +0100

    Expose the LibExeObjSteps from build.zig

--- build.zig
+++ build.zig
@@ -1,11 +1,22 @@
-const Builder = @import("std").build.Builder;
+const builtin = @import("builtin");
+const std = @import("std");
+
+const Builder = std.build.Builder;
+const LibExeObjStep = std.build.LibExeObjStep;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("tm35-rand-wild", "src/main.zig");
-    exe.setBuildMode(mode);
-    exe.addPackagePath("tm35-format", "lib/tm35-format/src/index.zig");
-    exe.addPackagePath("zig-clap", "lib/zig-clap/index.zig");
-    b.default_step.dependOn(&exe.step);
-    b.installArtifact(exe);
+    const exs = exes(b, b.build_root);
+    for (exs) |exe| {
+        exe.setBuildMode(mode);
+        b.default_step.dependOn(&exe.step);
+        b.installArtifact(exe);
+    }
+}
+
+pub fn exes(b: *Builder, path: []const u8) [1]*LibExeObjStep {
+    const exe = b.addExecutable("tm35-rand-wild", b.fmt("{}/src/main.zig", path));
+    exe.addPackagePath("tm35-format", b.fmt("{}/lib/tm35-format/src/index.zig", path));
+    exe.addPackagePath("zig-clap", b.fmt("{}/lib/zig-clap/index.zig", path));
+    return []*LibExeObjStep{exe};
 }
