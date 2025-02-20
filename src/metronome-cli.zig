330baeda008451cdc98a443e4de2d433a29cdcf4
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-02-20 14:42:23 +0100

    WIP: Cli --help

--- src/metronome-cli.zig
+++ src/metronome-cli.zig
@@ -14,6 +14,8 @@ pub fn main() !void {
     var input: ?[]const u8 = null;
     var output: ?[]const u8 = null;
     while (args.next()) {
+        if (args.flag(&.{ "-h", "--help" }))
+            return printHelp(std.io.getStdOut());
         if (args.option(&.{ "-i", "--input" })) |i|
             input = i;
         if (args.option(&.{ "-o", "--output" })) |o|
@@ -24,21 +26,13 @@ pub fn main() !void {
             break;
     }
 
-    var input_file = try std.fs.cwd().openFile(input orelse return error.NoInput, .{});
-    defer input_file.close();
-    var output_file = try std.fs.cwd().createFile(output orelse return error.NoOutput, .{});
-    defer output_file.close();
-
-    var game = try core.Game.fromFile(input_file, gpa);
-    defer game.deinit();
-
     var commands = Metronome.Commands{};
 
     while (args.next()) {
         const command_name = args.positional().?;
 
         inline for (Metronome.Commands.descriptions) |command_description| loop_blk: {
-            if (!std.mem.eql(u8, command_name, command_description.name))
+            if (!std.mem.eql(u8, command_name, command_description.id))
                 break :loop_blk; // TODO: We cannot use `continue` in `inline for`
 
             var command_outer = @unionInit(Metronome.Command, command_description.id, .{});
@@ -46,7 +40,8 @@ pub fn main() !void {
 
             while (args.next()) {
                 if (args.flag(&.{ "-h", "--help" })) {
-                    // TODO: Print help
+                    try printCommandHelp(std.io.getStdOut(), command_description);
+                    return;
                 }
 
                 var buf: [128]u8 = undefined;
@@ -68,6 +63,14 @@ pub fn main() !void {
         }
     }
 
+    var input_file = try std.fs.cwd().openFile(input orelse return error.NoInput, .{});
+    defer input_file.close();
+    var output_file = try std.fs.cwd().createFile(output orelse return error.NoOutput, .{});
+    defer output_file.close();
+
+    var game = try core.Game.fromFile(input_file, gpa);
+    defer game.deinit();
+
     var random = std.Random.DefaultPrng.init(seed orelse std.crypto.random.int(u64));
     var metronome = try Metronome.init(gpa, arena, random.random());
     switch (game) {
@@ -76,6 +79,46 @@ pub fn main() !void {
 
     try game.apply();
     try game.write(output_file.writer());
+}
+
+fn printHelp(file: std.fs.File) !void {
+    const writer = file.writer();
+
+    try writer.writeAll(
+        \\A tool for modifying and randomizing Pokémon game roms.
+        \\
+        \\Usage: metronome-cli [OPTIONS] [COMMAND]...
+        \\
+        \\Options:
+        \\  -i, --input <path>
+        \\  -o, --output <path>
+        \\  -s, --seed <seed>
+        \\
+    );
+}
+
+fn printCommandHelp(file: std.fs.File, description: Metronome.Command.Description) !void {
+    const writer = file.writer();
+
+    try writer.writeAll(description.description);
+    try writer.writeAll("\n\n");
+    try writer.writeAll("Usage: metronome-cli ");
+    try writer.writeAll(description.id);
+    try writer.writeAll(" [OPTIONS] [COMMAND]...\n\nOptions:\n");
+
+    // pub const Description = struct {
+    //     id: []const u8,
+    //     name: []const u8,
+    //     description: []const u8,
+    //     options: []const OptionDescription,
+    // };
+
+    // pub const OptionDescription = struct {
+    //     id: []const u8,
+    //     name: []const u8,
+    //     description: []const u8,
+    // };
+
 }
 
 fn parseString(comptime T: type, string: []const u8) !T {
